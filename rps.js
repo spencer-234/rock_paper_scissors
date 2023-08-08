ca9345e5ea80b1a9f1ecd537055ac12600a73532
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2023-08-08 14:25:41 -0400

    add playRound and game functions

--- rps.js
+++ rps.js
@@ -1,17 +1,42 @@
-// get input from user
-let playerChoice;
-let playerChoices = ["rock", "paper", "scissors"];
-while (playerChoices.indexOf(playerChoice) === -1) {
-    playerChoice = prompt("Select 'rock', 'paper', or 'scissors': ").toLowerCase();
-};
-
 // get computer choice
-function computerChoice() {
+function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
-    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    let choice= choices[Math.floor(Math.random() * choices.length)];
     return choice;
 };
 
+
 // compare two choices to declare winner
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors.";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock.";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats paper.";
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "You lose! Rock beats scissors.";
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        return "You lose! Paper beats rock.";
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        return "You lose! Scissors beats paper.";
+    } else {
+        return "It's a tie!";
+    }
+};
 
 // loop 5 times and declare winner
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection);
+    }
+};
+
+// get input from user
+let playerSelection;
+let playerChoices = ["rock", "paper", "scissors"];
+while (playerChoices.indexOf(playerSelection) === -1) {
+    playerSelection = prompt("Select 'rock', 'paper', or 'scissors': ").toLowerCase();
+}
+
+let computerSelection = getComputerChoice();
