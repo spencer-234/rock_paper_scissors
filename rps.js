db0003117ea07181e0e8de5bfe6b282e1c9d9c90
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2023-08-08 14:43:43 -0400

    complete game function

--- rps.js
+++ rps.js
@@ -27,8 +27,25 @@ function playRound(playerSelection, computerSelection) {
 
 // loop 5 times and declare winner
 function game() {
+    let playerWins = 0;
+    let computerWins = 0;
     for (let i = 0; i < 5; i++) {
-        playRound(playerSelection, computerSelection);
+        let round = playRound(playerSelection, computerSelection);
+        if ((round === "You win! Rock beats scissors.") || (round === "You win! Paper beats rock.") || (round === "You win! Scissors beats paper.")) {
+            playerWins++;
+        } else if (round === "It's a tie!") {
+            computerWins += 0;
+        } else {
+            computerWins++;
+        }
+    }
+    
+    if (playerWins > computerWins) {
+        return "You Win!";
+    } else if (playerWins < computerWins) {
+        return "You lose!";
+    } else {
+        return "It's a tie!";
     }
 };
 
